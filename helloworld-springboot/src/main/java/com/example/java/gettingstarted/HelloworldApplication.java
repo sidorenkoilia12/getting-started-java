fd4aa5bf923d0c6d8e2ed3282513b44d084036e5
Ilya <sidorenkoilia12@gmail.com>
2018-04-12 14:21:52 +0300

    just for repeat

--- helloworld-springboot/src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
+++ helloworld-springboot/src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldApplication {
   @RequestMapping("/")
   public String home() {
-    return "Hello World!";
+    return "Hello World and CircleCI!";
   }
 
   /**
